a0ac62674295b0a344018d9a96e3f577bdbd0403
Ronald DC <rcdelacruz@gmail.com>
2022-06-09 18:00:00 +0800

    first commit

---
+++ pkg/books/get_books.go
@@ -0,0 +1,19 @@
+package books
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rcdelacruz/go-gin-api/pkg/common/models"
+)
+
+func (h handler) GetBooks(c *gin.Context) {
+	var books []models.Book
+
+	if result := h.DB.Find(&books); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusOK, &books)
+}
